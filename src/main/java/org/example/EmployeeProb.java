64452b19b06a5183838ca45714a0b393bf0eaa89
subham <subham10022000@gmail.com>
2025-05-04 21:30:05 +0530

    changes

--- src/main/java/org/example/EmployeeProb.java
+++ src/main/java/org/example/EmployeeProb.java
@@ -1,2 +1,50 @@
-package org.example;public class EmployeeProb {
+package org.example;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class EmployeeProb {
+    public static void main(String[] args) {
+
+
+        List<Employee> list = new ArrayList<>();
+        list.add(new Employee(1,"Subham",1000));
+        list.add(new Employee(2,"Prakash",2000));
+        list.add(new Employee(3,"Sudeep",3000));
+        list.add(new Employee(4,"Ankit",1500));
+
+        Optional<Employee> find = list.stream()
+                                  .sorted().skip(1)
+                                  .findFirst();
+
+        if(!find.isEmpty()){
+            System.out.println(find.get().name);
+        }
+    }
 }
+
+class Employee implements Comparable<Employee>{
+
+    int emp_id;
+    String name;
+    int salary;
+
+
+    Employee(int emp_id,String name,int salary){
+        this.emp_id = emp_id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+
+    @Override
+    public int compareTo(Employee o) {
+
+        if(this.salary < o.salary){
+            return -1;
+        }else{
+            return 1;
+        }
+    }
+}
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
     }
 }
+
+// SQL qsn
+
+
+
+
+
