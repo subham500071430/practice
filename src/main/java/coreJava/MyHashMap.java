005c2c8589afb7a279e6e4e1f34a8210ff9befd6
subham <subham10022000@gmail.com>
2025-06-03 15:17:50 +0530

    update

--- src/main/java/coreJava/MyHashMap.java
+++ src/main/java/coreJava/MyHashMap.java
@@ -5,80 +5,92 @@ import java.util.LinkedList;
 public class MyHashMap<K, V> {
 
     private int initial_capacity = 16;
-    private double load_factor = 0.75d;
+    private double load_factor = 0.75;
     private LinkedList<Node<K, V>>[] map;
     private int size;
 
-    MyHashMap() {
-        map = new LinkedList[this.initial_capacity];
+    public MyHashMap() {
+        map = new LinkedList[initial_capacity];
     }
 
-    MyHashMap(int intitial_capacity) {
-        this.initial_capacity = intitial_capacity;
-        map = new LinkedList[intitial_capacity];
+    public MyHashMap(int initial_capacity) {
+        this.initial_capacity = initial_capacity;
+        map = new LinkedList[initial_capacity];
     }
 
-    MyHashMap(int intitial_capacity, int load_factor) {
-        this.initial_capacity = intitial_capacity;
+    public MyHashMap(int initial_capacity, double load_factor) {
+        this.initial_capacity = initial_capacity;
         this.load_factor = load_factor;
-        map = new LinkedList[intitial_capacity];
+        map = new LinkedList[initial_capacity];
     }
 
     public V get(K key) {
         int index = hash(key);
-        if(map[index] == null)
-             return null;
+        if (map[index] == null) return null;
 
-        for (Node node : map[index]) {
-            if (node.key == key) {
-                return (V) node.value;
+        for (Node<K, V> node : map[index]) {
+            if (node.key.equals(key)) {
+                return node.value;
             }
         }
         return null;
     }
 
     public void put(K key, V value) {
-
         int index = hash(key);
 
         if (map[index] == null) {
             map[index] = new LinkedList<>();
-            map[index].add(new Node(key, value));
-            size++;
-        } else {
-            for (Node node : map[index]) {
-                if (node.key == key) {
-                    node.value = value;
-                }
+        }
+
+        for (Node<K, V> node : map[index]) {
+            if (node.key.equals(key)) {
+                node.value = value;
+                return;
             }
         }
+
+        map[index].add(new Node<>(key, value));
+        size++;
 
         if (size >= (int) (load_factor * map.length)) {
             resize();
         }
     }
 
-    public boolean containsKey(K key){
-           int index = hash(key);
-           return map[index] != null;
+    public boolean containsKey(K key) {
+        int index = hash(key);
+        if (map[index] == null) return false;
+
+        for (Node<K, V> node : map[index]) {
+            if (node.key.equals(key)) return true;
+        }
+        return false;
     }
 
     public int size() {
         return size;
     }
 
-    public void resize() {
-        LinkedList<Node<K, V>>[] newMap = new LinkedList[2 * map.length];
-        System.arraycopy(map, 0, newMap, 0, newMap.length);
-        map = newMap;
+    private void resize() {
+        LinkedList<Node<K, V>>[] oldMap = map;
+        map = new LinkedList[2 * oldMap.length];
+        size = 0;
+
+        for (LinkedList<Node<K, V>> bucket : oldMap) {
+            if (bucket != null) {
+                for (Node<K, V> node : bucket) {
+                    put(node.key, node.value);
+                }
+            }
+        }
     }
 
-    public int hash(K key) {
-        return (int) key % map.length;
+    private int hash(K key) {
+        return (key.hashCode() & 0x7fffffff) % map.length;
     }
 
     static class Node<K, V> {
-
         K key;
         V value;
 
@@ -87,6 +99,4 @@ public class MyHashMap<K, V> {
             this.value = value;
         }
     }
-
-
 }
