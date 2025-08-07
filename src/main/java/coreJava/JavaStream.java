4e467a9d9ec6c08d0c8409a43fd54a644fca8a1c
subham <subham10022000@gmail.com>
2025-08-07 09:10:40 +0530

    java 8 streams practice

--- src/main/java/coreJava/JavaStream.java
+++ src/main/java/coreJava/JavaStream.java
@@ -2,35 +2,34 @@ package coreJava;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class JavaStream {
 
     public static void main(String[] args) {
 
-           List<Employee> list = new ArrayList<>();
+        List<Employee> list = new ArrayList<>();
 
-           list.add(new Employee(1,100));
-           list.add(new Employee(2,200));
-           list.add(new Employee(3,300));
+        list.add(new Employee(1, 100));
+        list.add(new Employee(2, 200));
+        list.add(new Employee(3, 300));
 
-           List<Employee> res = list.stream()
-                   .map(employee -> new Employee(employee.empId , 1.1*employee.salary))
-                   .toList();
+        List<Employee> res = list.stream()
+                .map(employee -> new Employee(employee.empId, 1.1 * employee.salary))
+                .toList();
 
-           res.forEach(employee -> System.out.println(employee.salary));
+        res.forEach(employee -> System.out.println(employee.salary));
 
     }
 }
 
 class Employee {
 
-      int empId;
-      double salary;
+    int empId;
+    double salary;
 
-      Employee(int empId,double salary) {
-          this.empId = empId;
-          this.salary = salary;
-      }
+    Employee(int empId, double salary) {
+        this.empId = empId;
+        this.salary = salary;
+    }
 }
 
